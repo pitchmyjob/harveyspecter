e0bbcbdb8a3f03eb4a42156432d763e8f45d92fb
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 15:13:08 +0100

    Allows to retrieve user data from authentication token

--- src/user/UserActions.js
+++ src/user/UserActions.js
@@ -1,6 +1,6 @@
 import request from '../utils/request'
 
-import { ACTIVATE_USER, LOGIN_USER } from './UserConstants'
+import { ACTIVATE_USER, LOGIN_USER, RETRIEVE_FROM_TOKEN_USER } from './UserConstants'
 
 export function activateUser(email, token) {
     return {
@@ -13,5 +13,14 @@ export function loginUser(values) {
     return {
         type: LOGIN_USER,
         payload: request.post('/auth/pro/login/', values)
+    }
+}
+
+export const retrieveUserFromToken = (token) => {
+    request.defaults.headers.common['Authorization'] = 'Token ' + token;
+
+    return {
+        type: RETRIEVE_FROM_TOKEN_USER,
+        payload: request.get('/auth/me/')
     }
 }
--- src/user/UserReducer.js
+++ src/user/UserReducer.js
@@ -1,8 +1,12 @@
-import { ACTIVATE_USER_PENDING, ACTIVATE_USER_FULFILLED, ACTIVATE_USER_REJECTED } from './UserConstants'
+import { ACTIVATE_USER_PENDING, ACTIVATE_USER_FULFILLED, ACTIVATE_USER_REJECTED,
+         RETRIEVE_FROM_TOKEN_USER_PENDING, RETRIEVE_FROM_TOKEN_USER_FULFILLED, RETRIEVE_FROM_TOKEN_USER_REJECTED
+} from './UserConstants'
 
 const INITIAL_STATE = {
     activating: false,
     activated: false,
+    fetching: false,
+    fetched: false,
     error: null,
     currentUser: null,
 }
@@ -16,6 +20,14 @@ export default (state=INITIAL_STATE, action) => {
             return {...state, activating: false, activated: true}
         case ACTIVATE_USER_REJECTED:
             return {...state, activating: false, error: action.payload.response.data}
+
+        // RETRIEVE FROM TOKEN
+        case RETRIEVE_FROM_TOKEN_USER_PENDING:
+            return {...state, fetching: true}
+        case RETRIEVE_FROM_TOKEN_USER_FULFILLED:
+            return {...state, fetching: false, fetched: true, currentUser: action.payload.data}
+        case RETRIEVE_FROM_TOKEN_USER_REJECTED:
+            return {...state, fetching: false, error: action.payload.response.data}
 
         // DEFAULT
         default:

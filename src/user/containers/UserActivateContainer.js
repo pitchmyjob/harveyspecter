4ae5cbe3cc5bc316800ef815585b29f1cdcdd60b
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 14:32:28 +0100

    Removes unused import

--- src/user/containers/UserActivateContainer.js
+++ src/user/containers/UserActivateContainer.js
@@ -2,7 +2,6 @@ import { connect } from 'react-redux'
 
 import UserActivate from '../components/UserActivate'
 import { activateUser } from '../UserActions'
-import { formatErrors  } from '../../utils/forms/formatters'
 
 const mapStateToProps = (state) => {
     return {

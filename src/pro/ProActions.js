2b71b49aa4abe98ea282c223c8613ef2a3cc509c
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 18:23:08 +0100

    Allows to handle pro update form

--- src/pro/ProActions.js
+++ src/pro/ProActions.js
@@ -1,10 +1,26 @@
 import request from '../utils/request'
 
-import { CREATE_PRO } from './ProConstants'
+import { RETRIEVE_PRO, CREATE_PRO, UPDATE_PRO } from './ProConstants'
+
+export function retrievePro(values) {
+    return {
+        type: RETRIEVE_PRO,
+        payload: request.get('/pro/me/')
+    }
+}
 
 export function createPro(values) {
     return {
         type: CREATE_PRO,
         payload: request.post('/auth/pro/register/', values)
+    }
+}
+
+export function updatePro(values) {
+    delete values['logo']
+
+    return {
+        type: UPDATE_PRO,
+        payload: request.put('/pro/me/', values)
     }
 }

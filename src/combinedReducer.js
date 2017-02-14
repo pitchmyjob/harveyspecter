cec687021b6d47148e1206ee018933838ab36656
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 16:21:14 +0100

    Adds "employee" app to be able to retrieve employee types

--- src/combinedReducer.js
+++ src/combinedReducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
 import AlertReducer from './alert/AlertReducer'
+import EmployeeReducer from './pro/EmployeeReducer'
 import ProReducer from './pro/ProReducer'
 import UserReducer from './user/UserReducer'
 
 const combinedReducer = combineReducers({
     form : formReducer,
     alert: AlertReducer,
+    employee: EmployeeReducer,
     pro: ProReducer,
     user: UserReducer,
 })

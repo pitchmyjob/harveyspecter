12c8e597f9eec552f2cbb42533a8056230f592d3
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 14:33:02 +0100

    Adds user reducer to combined reducer

--- src/combinedReducer.js
+++ src/combinedReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
 import ProReducer from './pro/ProReducer'
+import UserReducer from './user/UserReducer'
 
 const combinedReducer = combineReducers({
     form : formReducer,
     pro: ProReducer,
+    user: UserReducer,
 })
 
 export default combinedReducer

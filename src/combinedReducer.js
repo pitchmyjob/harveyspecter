3b40af46c545708a6b52facbf3868a19a431c23c
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 16:29:52 +0100

    Fix import name

--- src/combinedReducer.js
+++ src/combinedReducer.js
@@ -2,8 +2,8 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
 import AlertReducer from './alert/AlertReducer'
-import EmployeeReducer from './pro/EmployeeReducer'
-import IndustryReducer from './pro/IndustryReducer'
+import EmployeeReducer from './employee/EmployeeReducer'
+import IndustryReducer from './industry/IndustryReducer'
 import ProReducer from './pro/ProReducer'
 import UserReducer from './user/UserReducer'
 

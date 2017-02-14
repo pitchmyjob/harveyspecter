790ebea747152716e8f83a07552dc7cb743f263b
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 14:39:36 +0100

    Adds route to login form

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route } from 'react-router'
 
 import ProCreateFormContainer from './pro/containers/ProCreateFormContainer'
 import UserActivateContainer from './user/containers/UserActivateContainer'
+import UserLoginFormContainer from './user/containers/UserLoginFormContainer'
 
 export default (
     <div>
         <Route path="/register/" component={ProCreateFormContainer} />
         <Route path="/user/activate/:email/:token/" component={UserActivateContainer} />
+        <Route path="/login/" component={UserLoginFormContainer} />
     </div>
 )

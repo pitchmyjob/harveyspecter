c94bdacb8b2af868b6ceb80ccae5fd6b835403da
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 13:37:56 +0100

    Adds pro reducer to combinedReducer

--- src/combinedReducer.js
+++ src/combinedReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
+import ProReducer from './pro/ProReducer'
+
 const combinedReducer = combineReducers({
     form : formReducer,
+    pro: ProReducer,
 })
 
 export default combinedReducer

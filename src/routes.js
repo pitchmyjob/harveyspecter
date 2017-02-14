323b0ee94c83891fcb3b275459e89fc6145730fb
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 14:33:26 +0100

    Adds route to handle ACTIVATE_USER

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Route } from 'react-router'
 
 import ProCreateFormContainer from './pro/containers/ProCreateFormContainer'
+import UserActivateContainer from './user/containers/UserActivateContainer'
 
 export default (
     <div>
         <Route path="/register/" component={ProCreateFormContainer} />
+        <Route path="/user/activate/:email/:token/" component={UserActivateContainer} />
     </div>
 )

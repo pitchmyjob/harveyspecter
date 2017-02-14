f4ead39d659de5ff6fdddceeaedcbe42739fcec9
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 15:59:05 +0100

    Adds alert reducer to combined reducer

--- src/combinedReducer.js
+++ src/combinedReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
+import AlertReducer from './alert/AlertReducer'
 import ProReducer from './pro/ProReducer'
 import UserReducer from './user/UserReducer'
 
 const combinedReducer = combineReducers({
     form : formReducer,
+    alert: AlertReducer,
     pro: ProReducer,
     user: UserReducer,
 })

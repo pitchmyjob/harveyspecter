0f76ad3b2388b524e9396331f57360517cdf59fc
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-14 14:39:20 +0100

    Adds login functionnality

--- src/user/UserActions.js
+++ src/user/UserActions.js
@@ -1,10 +1,17 @@
 import request from '../utils/request'
 
-import { ACTIVATE_USER } from './UserConstants'
+import { ACTIVATE_USER, LOGIN_USER } from './UserConstants'
 
 export function activateUser(email, token) {
     return {
         type: ACTIVATE_USER,
         payload: request.put('/auth/pro/confirm/', {email, token})
+    }
+}
+
+export function loginUser(values) {
+    return {
+        type: LOGIN_USER,
+        payload: request.post('/auth/pro/login/', values)
     }
 }

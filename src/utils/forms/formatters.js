853ae6cf565cb5b5e61d2e9f118fca566d656e77
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-16 15:25:14 +0100

    Removes formatErrors function

--- src/utils/forms/formatters.js
+++ src/utils/forms/formatters.js
@@ -1,17 +1,7 @@
 import { SubmissionError } from 'redux-form'
 
-export const formatErrors = (errors) => {
-    let formattedErrors = {}
-    for (let key in errors) {
-        if (errors.hasOwnProperty(key)) {
-            let formatted_key = (key === 'non_field_errors' ? '_error' : key)
-            formattedErrors[formatted_key] = errors[key].join()
-        }
-    }
-    return formattedErrors
-}
-
 export const handleFormErrors = (response, handleNotFound=false) => {
+    console.log(response)
     if (response) {
         let formErrors = {}
 

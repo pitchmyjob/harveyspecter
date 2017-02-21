a6ae4b3166de5022b34f548a52fd8af043ae79c4
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-21 13:12:01 +0100

    Improves create pro form and user activate page design and errors

--- src/pro/components/ProCreateForm.js
+++ src/pro/components/ProCreateForm.js
@@ -18,71 +18,59 @@ export default class ProCreateForm extends React.Component {
         }
 
         return (
-            <div className="page-login-v3 layout-full">
-                <div className="page vertical-align text-xs-center" data-animsition-in="fade-in" data-animsition-out="fade-out">
-                    <div className="page-content vertical-align-middle animation-slide-top animation-duration-1">
-                        <div className="panel">
-                            <div className="panel-body">
-                                <div className="brand">
-                                    <img className="brand-img" src="https://s3-eu-west-1.amazonaws.com/spitchapp-dev/static/assets/images/logo-blue.png" alt="..." />
-                                    <h2 className="brand-text font-size-18">Spitch</h2>
-                                    <p>Sign up to find interesting thing</p>
-                                </div>
-                                <form onSubmit={handleSubmit} role="form" autoComplete="off">
-                                    <Field
-                                        name="company"
-                                        component={renderAuthInput}
-                                        type="text"
-                                        validate={isRequired}
-                                        placeholder="Raison sociale" />
-                                    <Field
-                                        name="last_name"
-                                        component={renderAuthInput}
-                                        type="text"
-                                        validate={isRequired}
-                                        placeholder="Nom" />
-                                    <Field
-                                        name="first_name"
-                                        component={renderAuthInput}
-                                        type="text"
-                                        validate={isRequired}
-                                        placeholder="Prénom" />
-                                    <Field
-                                        name="position"
-                                        component={renderAuthInput}
-                                        type="text"
-                                        placeholder="Fonction" />
-                                    <Field
-                                        name="phone"
-                                        component={renderAuthInput}
-                                        type="text"
-                                        placeholder="Téléphone" />
-                                    <Field
-                                        name="email"
-                                        component={renderAuthInput}
-                                        type="email"
-                                        validate={[isRequired, isEmail]}
-                                        placeholder="Adresse e-mail" />
-                                    <Field
-                                        name="password"
-                                        component={renderAuthInput}
-                                        type="password"
-                                        validate={[isRequired, minLength(6)]}
-                                        placeholder="Mot de passe" />
-                                    <Field
-                                        name="cgu"
-                                        component={renderAuthCheckbox}
-                                        validate={isRequired}
-                                        label="Accepter les conditions générales d'utilisation" />
-                                    <button type="submit" className="btn btn-primary btn-block" disabled={pristine || submitting}>Inscription</button>
-                                </form>
-                                <p>
-                                    Déjà inscrit ? <Link to="/login/">Connexion</Link>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <p>Sign up to find interesting thing</p>
+                <form onSubmit={handleSubmit} role="form" autoComplete="off">
+                    <Field
+                        name="company"
+                        component={renderAuthInput}
+                        type="text"
+                        validate={isRequired}
+                        placeholder="Raison sociale" />
+                    <Field
+                        name="last_name"
+                        component={renderAuthInput}
+                        type="text"
+                        validate={isRequired}
+                        placeholder="Nom" />
+                    <Field
+                        name="first_name"
+                        component={renderAuthInput}
+                        type="text"
+                        validate={isRequired}
+                        placeholder="Prénom" />
+                    <Field
+                        name="position"
+                        component={renderAuthInput}
+                        type="text"
+                        placeholder="Fonction" />
+                    <Field
+                        name="phone"
+                        component={renderAuthInput}
+                        type="text"
+                        placeholder="Téléphone" />
+                    <Field
+                        name="email"
+                        component={renderAuthInput}
+                        type="email"
+                        validate={[isRequired, isEmail]}
+                        placeholder="Adresse e-mail" />
+                    <Field
+                        name="password"
+                        component={renderAuthInput}
+                        type="password"
+                        validate={[isRequired, minLength(6)]}
+                        placeholder="Mot de passe" />
+                    <Field
+                        name="cgu"
+                        component={renderAuthCheckbox}
+                        validate={isRequired}
+                        label="Accepter les conditions générales d'utilisation" />
+                    <button type="submit" className="btn btn-primary btn-block" disabled={pristine || submitting}>Inscription</button>
+                </form>
+                <p>
+                    Déjà inscrit ? <Link to="/login/">Connexion</Link>
+                </p>
             </div>
         )
     }

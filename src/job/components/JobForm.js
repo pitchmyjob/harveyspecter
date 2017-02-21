5c783b789eb8c804a4cfde3ccd0aee3affab2d7f
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-21 17:56:00 +0100

    Improves design for job list and form

--- src/job/components/JobForm.js
+++ src/job/components/JobForm.js
@@ -26,71 +26,81 @@ export default class JobForm extends React.Component {
         const labelForm = this.props.params.hasOwnProperty('id') ? 'Modifier' : 'Ajouter'
 
         return (
-            <div>
-                <h3 className="font-size-24">{labelForm} une offre</h3>
-                <form onSubmit={handleSubmit} role="form" autoComplete="off">
-                    <Field
-                        name="logo"
-                        component={ImageBase64Field}
-                        validate={isRequired}
-                        label="Logo" />
-                    <Field
-                        name="title"
-                        component={renderInput}
-                        type="text"
-                        validate={isRequired}
-                        label="Titre de l'offre" />
-                    <Field
-                        name="address"
-                        component={LocationField}
-                        validate={isRequired}
-                        label="Lieu" />
-                    <Field
-                        name="starting_date"
-                        component={renderInput}
-                        type="text"
-                        validate={isRequired}
-                        label="Date de début" />
-                    <Field
-                        name="contract_types"
-                        component={renderSelectMultiple}
-                        options={contractType.contractTypes}
-                        validate={isRequired}
-                        label="Contrat(s)"
-                        placeholder="Choisissez un ou plusieurs contrats" />
-                    <Field
-                        name="experiences"
-                        component={renderSelectMultiple}
-                        options={experience.experiences}
-                        validate={isRequired}
-                        label="Expérience(s)"
-                        placeholder="Choisissez un ou plusieurs expériences" />
-                    <Field
-                        name="study_levels"
-                        component={renderSelectMultiple}
-                        options={studyLevel.studyLevels}
-                        validate={isRequired}
-                        label="Niveau d'études"
-                        placeholder="Choisissez un ou plusieurs niveau d'études" />
-                    <Field
-                        name="salary"
-                        component={renderInput}
-                        type="text"
-                        validate={isRequired}
-                        label="Salaire" />
-                    <Field
-                        name="skills"
-                        component={renderInput}
-                        type="text"
-                        validate={isRequired}
-                        label="Compétence(s) recherchée(s)" />
-                    <Field
-                        name="description"
-                        component={renderTextarea}
-                        validate={isRequired}
-                        label="Description" />
-                    <button type="submit" className="btn btn-primary btn-block" disabled={pristine || submitting}>{labelForm}</button>
-                </form>
+            <div className="page-content container-fluid">
+                <div className="row">
+                    <div className="col-xs-12">
+                        <div className="panel">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">Information entreprise</h3>
+                            </div>
+                            <div className="panel-body container-fluid">
+                                <form onSubmit={handleSubmit} role="form" autoComplete="off">
+                                    <Field
+                                        name="logo"
+                                        component={ImageBase64Field}
+                                        validate={isRequired}
+                                        label="Logo" />
+                                    <Field
+                                        name="title"
+                                        component={renderInput}
+                                        type="text"
+                                        validate={isRequired}
+                                        label="Titre de l'offre" />
+                                    <Field
+                                        name="address"
+                                        component={LocationField}
+                                        validate={isRequired}
+                                        label="Lieu" />
+                                    <Field
+                                        name="starting_date"
+                                        component={renderInput}
+                                        type="text"
+                                        validate={isRequired}
+                                        label="Date de début" />
+                                    <Field
+                                        name="contract_types"
+                                        component={renderSelectMultiple}
+                                        options={contractType.contractTypes}
+                                        validate={isRequired}
+                                        label="Contrat(s)"
+                                        placeholder="Choisissez un ou plusieurs contrats" />
+                                    <Field
+                                        name="experiences"
+                                        component={renderSelectMultiple}
+                                        options={experience.experiences}
+                                        validate={isRequired}
+                                        label="Expérience(s)"
+                                        placeholder="Choisissez un ou plusieurs expériences" />
+                                    <Field
+                                        name="study_levels"
+                                        component={renderSelectMultiple}
+                                        options={studyLevel.studyLevels}
+                                        validate={isRequired}
+                                        label="Niveau d'études"
+                                        placeholder="Choisissez un ou plusieurs niveau d'études" />
+                                    <Field
+                                        name="salary"
+                                        component={renderInput}
+                                        type="text"
+                                        validate={isRequired}
+                                        label="Salaire" />
+                                    <Field
+                                        name="skills"
+                                        component={renderInput}
+                                        type="text"
+                                        validate={isRequired}
+                                        label="Compétence(s) recherchée(s)" />
+                                    <Field
+                                        name="description"
+                                        component={renderTextarea}
+                                        validate={isRequired}
+                                        label="Description" />
+                                    <button type="submit" className="btn btn-primary btn-block" disabled={pristine || submitting}>{labelForm}</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/job/components/JobList.js
+++ src/job/components/JobList.js
@@ -26,10 +26,16 @@ export default class JobList extends React.Component {
             jobList = jobs.map((job) => {
                 return (
                     <tr key={job.id}>
-                        <td>
-                            {job.title}<br />
-                            {job.created} - {job.contract_types_extra.join(', ')}
+                        <td className="work-status">
+                            <span className="tag tag-primary">{job.state}</span>
                         </td>
+                        <td className="subject">
+                            <div className="table-content">
+                                <p className="blue-grey-500">{job.title}</p>
+                                <span className="blue-grey-400">{job.created} - {job.contract_types_extra.join(', ')}</span>
+                            </div>
+                        </td>
+                        <td></td>
                         <td>
                             <Link to={'/jobs/edit/' + job.id + '/'}>Modifier</Link>
                             <button onClick={() => destroyJob(job.id)}>Supprimer</button>
@@ -40,21 +46,73 @@ export default class JobList extends React.Component {
         }
 
         return (
-            <div>
-                <p>
-                    <Link to="/jobs/create/">Ajouter une offre</Link>
-                </p>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Annonces</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {jobList}
-                    </tbody>
-                </table>
+            <div className="app-work">
+                <div className="page-content">
+                    <div className="panel">
+                        <div className="panel-heading">
+                            <form className="panel-search-form" role="search" style={{paddingLeft: 0, marginLeft: 0}}>
+                                <div className="input-search">
+                                    <i className="input-search-icon md-search" aria-hidden="true"></i>
+                                    <input type="text" className="form-control" id="inputSearch" name="search" placeholder="Rechercher..." />
+                                </div>
+                            </form>
+                            <ul className="panel-info">
+                                <li>
+                                    <div className="num green-600">23</div>
+                                    <p>En cours</p>
+                                </li>
+                                <li>
+                                    <div className="num red-600">8</div>
+                                    <p>Expirées</p>
+                                </li>
+                                <li>
+                                    <div className="num orange-600">5</div>
+                                    <p>En attente</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <p>
+                            <Link to="/jobs/create/">Ajouter une offre</Link>
+                        </p>
+                        <div className="panel-body">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Status</th>
+                                        <th>Annonces</th>
+                                        <th>Candidatures</th>
+                                        <th></th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {jobList}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="panel-footer">
+                            <nav>
+                                <ul data-plugin="paginator" data-total="50" data-skin="pagination-gap" className="pagination pagination-gap">
+                                    <li className="pagination-prev page-item">
+                                        <a className="page-link" href="#">
+                                            <span>Previous</span>
+                                        </a>
+                                    </li>
+                                    <li className="pagination-items page-item" data-value="1"><a className="page-link" href="#">1</a></li>
+                                    <li className="pagination-items page-item" data-value="2"><a className="page-link" href="#">2</a></li>
+                                    <li className="pagination-items page-item" data-value="3"><a className="page-link" href="#">3</a></li>
+                                    <li className="pagination-items page-item" data-value="4"><a className="page-link" href="#">4</a></li>
+                                    <li className="pagination-items page-item active" data-value="5"><a className="page-link" href="#">5</a></li>
+                                    <li className="pagination-items page-item disabled">
+                                        <a className="page-link" href="#">
+                                            <span>Next</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

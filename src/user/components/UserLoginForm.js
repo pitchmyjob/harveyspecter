e4a3cd694067984dbac5a3fa2a4a6e8847f4cc61
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-21 13:44:50 +0100

    Improves the login form

--- src/user/components/UserLoginForm.js
+++ src/user/components/UserLoginForm.js
@@ -14,57 +14,30 @@ export default class UserLoginForm extends React.Component {
         const { handleSubmit, submitting, error } = this.props
 
         return (
-            <div className="page-login-v3 layout-full">
-                <div className="page vertical-align text-xs-center" data-animsition-in="fade-in" data-animsition-out="fade-out">
-                    <div className="page-content vertical-align-middle animation-slide-top animation-duration-1">
-                        <div className="panel">
-                            <div className="panel-body">
-                                <div className="brand">
-                                    <img className="brand-img" src="https://s3-eu-west-1.amazonaws.com/spitchapp-dev/static/assets/images/logo-blue.png" alt="..." />
-                                    <h2 className="brand-text font-size-18">Spitch</h2>
-                                    <p>Sign up to find interesting thing</p>
-                                </div>
-                                {error && <p>{error}</p>}
-                                <form onSubmit={handleSubmit} role="form" autoComplete="off">
-                                    <Field
-                                        name="email"
-                                        component={renderAuthInput}
-                                        type="email"
-                                        validate={[isRequired, isEmail]}
-                                        placeholder="Adresse e-mail" />
-                                    <Field
-                                        name="password"
-                                        component={renderAuthInput}
-                                        type="password"
-                                        validate={[isRequired, minLength(6)]}
-                                        placeholder="Mot de passe" />
-                                    <div className="form-group clearfix">
-                                        <Link className="pull-xs-right" to="/forget-password-request/">Mot de passe oublié ?</Link>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary btn-block btn-lg m-t-40 ladda-button" disabled={submitting}>Connexion</button>
-                                </form>
-                                <p>
-                                    Pas de compte ? <Link to="/register/">Inscrivez-vous</Link>
-                                </p>
-                            </div>
-                        </div>
-                        <footer className="page-copyright page-copyright-inverse">
-                            <p>WEBSITE BY Spitch</p>
-                            <p>© 2016. All RIGHT RESERVED.</p>
-                            <div className="social">
-                                <a className="btn btn-icon btn-pure">
-                                    <i className="icon bd-twitter" aria-hidden="true"></i>
-                                </a>
-                                <a className="btn btn-icon btn-pure">
-                                    <i className="icon bd-facebook" aria-hidden="true"></i>
-                                </a>
-                                <a className="btn btn-icon btn-pure" >
-                                    <i className="icon bd-google-plus" aria-hidden="true"></i>
-                                </a>
-                            </div>
-                        </footer>
+            <div>
+                <p>Sign in to find interesting thing</p>
+                {error && <p className="text-danger">{error}</p>}
+                <form onSubmit={handleSubmit} role="form" autoComplete="off">
+                    <Field
+                        name="email"
+                        component={renderAuthInput}
+                        type="email"
+                        validate={[isRequired, isEmail]}
+                        placeholder="Adresse e-mail" />
+                    <Field
+                        name="password"
+                        component={renderAuthInput}
+                        type="password"
+                        validate={[isRequired, minLength(6)]}
+                        placeholder="Mot de passe" />
+                    <div className="form-group clearfix">
+                        <Link className="pull-xs-right" to="/forget-password-request/">Mot de passe oublié ?</Link>
                     </div>
-                </div>
+                    <button type="submit" className="btn btn-primary btn-block btn-lg m-t-40 ladda-button" disabled={submitting}>Connexion</button>
+                </form>
+                <p>
+                    Pas de compte ? <Link to="/register/">Inscrivez-vous</Link>
+                </p>
             </div>
         )
     }

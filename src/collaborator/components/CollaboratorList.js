684ec13c9ccc537a7ebacb7351c7d91e3d0334b2
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-22 12:56:47 +0100

    Adds an loader and loadingerror component + intregates it to collaborator list on dashboard

--- src/collaborator/components/CollaboratorList.js
+++ src/collaborator/components/CollaboratorList.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import Loader from '../../core/components/Loader'
+import LoadingError from '../../core/components/LoadingError'
+
 export default class CollaboratorList extends React.Component {
     componentDidMount() {
         this.props.listCollaborator()
@@ -7,48 +10,44 @@ export default class CollaboratorList extends React.Component {
 
     render() {
         const { destroyCollaborator } = this.props
-        const { fetching, error, collaborators } = this.props.collaborator
+        const { fetched, error, collaborators } = this.props.collaborator
 
         if (error) {
-            return (
-                <li className="list-group-item">Erreur lors du chargement</li>
-            )
+            return <LoadingError />
         }
-        else if (fetching) {
-            return (
-                <li className="list-group-item">Chargement...</li>
-            )
-        }
-
-        const collaboratorList = collaborators.map((collaborator) => {
-            return (
-                <li className="list-group-item" key={collaborator.id}>
-                    <div className="media">
-                        <div className="media-left">
-                            <a className="avatar avatar-online" href="#">
-                                <img src={collaborator.photo} alt=""/>
-                            </a>
-                        </div>
-                        <div className="media-body">
-                            <div className="pull-xs-right"></div>
-                            <div>
-                                <span>{collaborator.first_name} {collaborator.last_name}</span>
+        else if (fetched) {
+            const collaboratorList = collaborators.map((collaborator) => {
+                return (
+                    <li className="list-group-item" key={collaborator.id}>
+                        <div className="media">
+                            <div className="media-left">
+                                <a className="avatar avatar-online" href="#">
+                                    <img src={collaborator.photo} alt=""/>
+                                </a>
                             </div>
-                            <small><button onClick={() => destroyCollaborator(collaborator.id)}>Supprimer</button></small>
+                            <div className="media-body">
+                                <div className="pull-xs-right"></div>
+                                <div>
+                                    <span>{collaborator.first_name} {collaborator.last_name}</span>
+                                </div>
+                                <small><button onClick={() => destroyCollaborator(collaborator.id)}>Supprimer</button></small>
+                            </div>
+                        </div>
+                    </li>
+                )
+            })
+
+            return (
+                <ul className="list-group list-group-dividered list-group-full h-250" data-plugin="scrollable">
+                    <div data-role="container">
+                        <div data-role="content">
+                            {collaboratorList}
                         </div>
                     </div>
-                </li>
+                </ul>
             )
-        })
+        }
 
-        return (
-            <ul className="list-group list-group-dividered list-group-full h-250" data-plugin="scrollable">
-                <div data-role="container">
-                    <div data-role="content">
-                        {collaboratorList}
-                    </div>
-                </div>
-            </ul>
-        )
+        return <Loader />
     }
 }

dcddc28f1ec00b3ff26a398e03b414c552949fd6
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-02-27 12:29:00 +0100

    Adds an 404 error page

--- src/core/components/App.js
+++ src/core/components/App.js
@@ -3,6 +3,10 @@ import React from 'react'
 import AlertContainer from '../../alert/containers/AlertContainer'
 
 export default class App extends React.Component {
+    componentWillMount() {
+        document.body.classList.remove('layout-full');
+    }
+
     componentDidMount() {
         this.props.retrieveUserFromToken()
     }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router'
 
 import { loginRequired, logoutRequired } from './utils/auth'
 
+import Error404 from './core/components/Error404'
 import LayoutLoggedContainer from './core/containers/LayoutLoggedContainer'
 import LayoutNotLoggedContainer from './core/containers/LayoutNotLoggedContainer'
 import AppContainer from './core/containers/AppContainer'
@@ -21,25 +22,28 @@ import UserLoginFormContainer from './user/containers/UserLoginFormContainer'
 import UserUpdateFormContainer from './user/containers/UserUpdateFormContainer'
 
 export default (
-    <Route path="/" component={AppContainer}>
-        <Route onEnter={loginRequired} component={LayoutLoggedContainer}>
-            <IndexRoute component={DashboardContainer} />
-            <Route path="/company/edit/" component={ProUpdateFormContainer} />
-            <Route path="/collaborators/create/" component={CollaboratorCreateFormContainer} />
-            <Route path="/me/edit/" component={UserUpdateFormContainer} />
-            <Route path="/jobs/create/" component={JobCreateFormContainer} />
-            <Route path="/jobs/edit/:id/" component={JobUpdateFormContainer} />
-            <Route path="/jobs/:id/candidacies/" component={CandidacyListContainer} />
-            <Route path="/jobs/" component={JobListContainer}>
-                <Route path="/jobs/:page/" />
+    <div>
+        <Route path="/" component={AppContainer}>
+            <Route onEnter={loginRequired} component={LayoutLoggedContainer}>
+                <IndexRoute component={DashboardContainer} />
+                <Route path="/company/edit/" component={ProUpdateFormContainer} />
+                <Route path="/collaborators/create/" component={CollaboratorCreateFormContainer} />
+                <Route path="/me/edit/" component={UserUpdateFormContainer} />
+                <Route path="/jobs/create/" component={JobCreateFormContainer} />
+                <Route path="/jobs/edit/:id/" component={JobUpdateFormContainer} />
+                <Route path="/jobs/:id/candidacies/" component={CandidacyListContainer} />
+                <Route path="/jobs/" component={JobListContainer}>
+                    <Route path="/jobs/:page/" />
+                </Route>
+            </Route>
+            <Route onEnter={logoutRequired} component={LayoutNotLoggedContainer}>
+                <Route path="/register/" component={ProCreateFormContainer} />
+                <Route path="/user/activate/:email/:token/" component={UserActivateContainer} />
+                <Route path="/login/" component={UserLoginFormContainer} />
+                <Route path="/forget-password-confirm/:email/:token/" component={UserForgetPasswordConfirmFormContainer} />
+                <Route path="/forget-password-request/" component={UserForgetPasswordRequestFormContainer} />
             </Route>
         </Route>
-        <Route onEnter={logoutRequired} component={LayoutNotLoggedContainer}>
-            <Route path="/register/" component={ProCreateFormContainer} />
-            <Route path="/user/activate/:email/:token/" component={UserActivateContainer} />
-            <Route path="/login/" component={UserLoginFormContainer} />
-            <Route path="/forget-password-confirm/:email/:token/" component={UserForgetPasswordConfirmFormContainer} />
-            <Route path="/forget-password-request/" component={UserForgetPasswordRequestFormContainer} />
-        </Route>
-    </Route>
+        <Route path="*" component={Error404} />
+    </div>
 )

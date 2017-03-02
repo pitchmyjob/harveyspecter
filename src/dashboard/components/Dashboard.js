38f2fe72b62e629c11431d9c9d5caf9def32ab6a
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-02 18:11:20 +0100

    Rewrites dashboard component and container

--- src/dashboard/components/Dashboard.js
+++ src/dashboard/components/Dashboard.js
@@ -1,96 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router'
 
-import CollaboratorListContainer from '../../collaborator/containers/CollaboratorListContainer'
+import ProCard from './ProCard'
+import JobCounterCard from './JobCounterCard'
+import CollaboratorCard from './CollaboratorCard'
 
 export default class Dashboard extends React.Component {
+    componentDidMount() {
+        this.props.retrievePro()
+        this.props.retrieveCounterJob()
+        this.props.listCollaborator()
+    }
+
     render() {
+        const { destroyCollaborator } = this.props
+        const { pro, jobCounter, collaborator, currentUser } = this.props
+
         return (
             <div className="page-content container-fluid">
                 <div className="row" data-plugin="matchHeight" data-by-row="true">
                     <div className="col-xxl-4 col-xl-4 col-lg-12 col-xs-12">
-                        <div className="card card-shadow">
-                            <div className="card-block text-xs-center bg-white p-40">
-                                <div className="avatar avatar-100 m-b-20">
-                                    <img src="https://s3-eu-west-1.amazonaws.com/spitchapp-dev/static/base/assets/images/logopitch.png" alt="" />
-                                </div>
-                                <p className="font-size-20 blue-grey-700">Spitch</p>
-                                <p className="blue-grey-400 m-b-20">Recrutement</p>
-                                <p className="m-b-10">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                                    nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-                                </p>
-                                <p className="m-b-35">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                                    nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-                                </p>
-                                <ul className="list-inline font-size-18 m-b-35">
-                                    <li className="list-inline-item">
-                                        <a className="blue-grey-400" href="#">
-                                            <i className="icon bd-twitter" aria-hidden="true"></i>
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item m-l-10">
-                                        <a className="blue-grey-400" href="#">
-                                            <i className="icon bd-facebook" aria-hidden="true"></i>
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item m-l-10">
-                                        <a className="blue-grey-400" href="#">
-                                            <i className="icon bd-dribbble" aria-hidden="true"></i>
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item m-l-10">
-                                        <a className="blue-grey-400" href="#">
-                                            <i className="icon bd-instagram" aria-hidden="true"></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <Link to="/company/edit/">
-                                    <button type="button" className="btn btn-primary p-x-40">Modifier</button>
-                                </Link>
-                            </div>
-                        </div>
+                        <ProCard pro={pro} />
                     </div>
-                    <div className="col-xxl-8 col-xl-8 col-lg-12 col-xs-12" >
-                        <div className="col-xxl-4 col-xl-4 col-lg-12 col-xs-12" >
-                            <div className="card card-block p-10 bg-blue-600" >
-                                <div className="counter counter-lg counter-inverse">
-                                    <div className="counter-label text-uppercase">Offres en cours</div>
-                                    <span className="counter-number">220</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xxl-4 col-xl-4 col-lg-12 col-xs-12">
-                            <div className="card card-block p-10 bg-purple-600">
-                                <div className="counter counter-lg counter-inverse">
-                                    <div className="counter-label text-uppercase">Offres en attente</div>
-                                    <span className="counter-number">220</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xxl-4 col-xl-4 col-lg-12 col-xs-12">
-                            <div className="card card-block p-10 bg-red-600">
-                                <div className="counter counter-lg counter-inverse">
-                                    <div className="counter-label text-uppercase">Offres expirées</div>
-                                    <span className="counter-number">220</span>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="col-xxl-8 col-xl-8 col-lg-12 col-xs-12">
+                        <JobCounterCard counter={jobCounter} />
                         <div className="col-xl-6 col-lg-6 col-xs-12">
-                            <div className="panel" id="followers">
-                                <div className="panel-heading">
-                                    <h3 className="panel-title">
-                                        Collaborateurs
-                                    </h3>
-                                </div>
-                                <div className="panel-body">
-                                    <CollaboratorListContainer />
-                                    <Link to="/collaborators/create/">
-                                        <button type="button" className="btn btn-block btn-primary">Ajouter</button>
-                                    </Link>
-                                </div>
-                            </div>
+                            <CollaboratorCard currentUser={currentUser} collaborators={collaborator} onDelete={destroyCollaborator} />
                         </div>
                         <div className="col-xl-6 col-lg-6 col-xs-12">
                             <div className="panel" id="daily-feed">
--- src/dashboard/containers/DashboardContainer.js
+++ src/dashboard/containers/DashboardContainer.js
@@ -1,5 +1,37 @@
 import { connect } from 'react-redux'
 
 import Dashboard from '../components/Dashboard'
+import { retrieveCounterJob } from '../../job/JobActions'
+import { retrievePro } from '../../pro/ProActions'
+import { listCollaborator, destroyCollaborator } from '../../collaborator/CollaboratorActions'
+import { addAlertSuccess } from '../../alert/AlertActions'
 
-export default connect(null, null)(Dashboard)
+const mapStateToProps = (state) => {
+    return {
+        pro: state.pro,
+        jobCounter: state.job.jobCounter,
+        collaborator: state.collaborator,
+        currentUser: state.user.currentUser,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        retrievePro: () => {
+            return dispatch(retrievePro())
+        },
+        retrieveCounterJob: () => {
+            return dispatch(retrieveCounterJob())
+        },
+        listCollaborator: () => {
+            return dispatch(listCollaborator())
+        },
+        destroyCollaborator: (id) => {
+            return dispatch(destroyCollaborator(id)).then((response) => {
+                dispatch(addAlertSuccess('Collaborateur supprimé'))
+            })
+        },
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)

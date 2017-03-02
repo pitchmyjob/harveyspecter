a93db6ff22b149aa8441dbf2b3302e434c01941b
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-02 14:58:00 +0100

    Removes useless /

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import LayoutLoggedContainer from './core/containers/LayoutLoggedContainer'
 import LayoutNotLoggedContainer from './core/containers/LayoutNotLoggedContainer'
 import AppContainer from './core/containers/AppContainer'
 import CandidacyListContainer from './candidacy/containers/CandidacyListContainer'
+import SliderPanelCandidacy from './candidacy/components/SliderPanelCandidacy'
 import CollaboratorCreateFormContainer from './collaborator/containers/CollaboratorCreateFormContainer'
 import DashboardContainer from './dashboard/containers/DashboardContainer'
 import JobCreateFormContainer from './job/containers/JobCreateFormContainer'
@@ -28,22 +29,22 @@ export default (
         <Route path="/" component={AppContainer}>
             <Route onEnter={loginRequired} component={LayoutLoggedContainer}>
                 <IndexRoute component={DashboardContainer} />
-                <Route path="/company/edit/" component={ProUpdateFormContainer} />
-                <Route path="/collaborators/create/" component={CollaboratorCreateFormContainer} />
-                <Route path="/me/edit/" component={UserUpdateFormContainer} />
-                <Route path="/jobs/create/" component={JobCreateFormContainer} />
-                <Route path="/jobs/edit/:id/" component={JobUpdateFormContainer} />
-                <Route path="/jobs/:jobId/candidacies/:status/(:page/)" component={CandidacyListContainer} />
-                <Route path="/jobs/:jobId/question/" component={JobQuestionFormContainer} />
-                <Route path="/jobs/:id/publish/" component={JobPublishFormContainer} />
-                <Route path="/jobs/(:page/)" component={JobListContainer} />
+                <Route path="company/edit/" component={ProUpdateFormContainer} />
+                <Route path="collaborators/create/" component={CollaboratorCreateFormContainer} />
+                <Route path="me/edit/" component={UserUpdateFormContainer} />
+                <Route path="jobs/create/" component={JobCreateFormContainer} />
+                <Route path="jobs/edit/:id/" component={JobUpdateFormContainer} />
+                <Route path="jobs/:jobId/candidacies/:status/(:page/)" component={CandidacyListContainer} />
+                <Route path="jobs/:jobId/question/" component={JobQuestionFormContainer} />
+                <Route path="jobs/:id/publish/" component={JobPublishFormContainer} />
+                <Route path="jobs/(:page/)" component={JobListContainer} />
             </Route>
             <Route onEnter={logoutRequired} component={LayoutNotLoggedContainer}>
-                <Route path="/register/" component={ProCreateFormContainer} />
-                <Route path="/user/activate/:email/:token/" component={UserActivateContainer} />
-                <Route path="/login/" component={UserLoginFormContainer} />
-                <Route path="/forget-password-confirm/:email/:token/" component={UserForgetPasswordConfirmFormContainer} />
-                <Route path="/forget-password-request/" component={UserForgetPasswordRequestFormContainer} />
+                <Route path="register/" component={ProCreateFormContainer} />
+                <Route path="user/activate/:email/:token/" component={UserActivateContainer} />
+                <Route path="login/" component={UserLoginFormContainer} />
+                <Route path="forget-password-confirm/:email/:token/" component={UserForgetPasswordConfirmFormContainer} />
+                <Route path="forget-password-request/" component={UserForgetPasswordRequestFormContainer} />
             </Route>
         </Route>
         <Route path="*" component={Error404} />

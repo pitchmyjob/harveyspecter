6f8b462f4d0d7b5fe9256325fa722a386b5401a8
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-03 15:51:32 +0100

    Adds notification into navbar

--- src/core/containers/NavBarContainer.js
+++ src/core/containers/NavBarContainer.js
@@ -1,11 +1,25 @@
 import { connect } from 'react-redux'
 
 import NavBar from '../components/NavBar'
+import { listNotification, retrieveCounterNotification } from '../../notification/NotificationActions'
 
 const mapStateToProps = (state) => {
     return {
         currentUser: state.user.currentUser,
+        notificationList: state.notification.notificationList,
+        notificationCounter: state.notification.notificationCounter,
     }
 }
 
-export default connect(mapStateToProps, null)(NavBar)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        listNotification: () => {
+            return dispatch(listNotification())
+        },
+        retrieveCounterNotification: () => {
+            return dispatch(retrieveCounterNotification())
+        },
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
